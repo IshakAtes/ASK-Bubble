9b9d6f869abe5ce92e1a7c237adb7ecb056e4515
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-05-27 15:55:16 +0200

    Login validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgStyle } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormBuilder, FormControlName, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NgStyle, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  isPressed = false;
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      pw: ['', [Validators.required, Validators.minLength(5)]],
+      mail: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {}
+
+  onMouseDown() {
+    this.isPressed = true;
+  }
+
+  onMouseUp() {
+    this.isPressed = false;
+  }
 
 }
