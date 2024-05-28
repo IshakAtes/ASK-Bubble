9fab941c1b0bd5b91baf8451b5bb9503bf834f10
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-05-28 13:27:48 +0200

    added imprint, privacy-policy & signUp Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 import { LoginComponent } from './login/login.component';
 import { ChannelComponent } from './channel/channel.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'chat', component: ChatComponent },
-    { path: 'channel', component: ChannelComponent}
+    { path: 'channel', component: ChannelComponent},
+    { path: 'imprint', component: ImprintComponent},
+    { path: 'privacyPolicy', component: PrivacyPolicyComponent},
+    { path: 'signUp', component: SignUpComponent}
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule, NgStyle } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { FormBuilder, FormControlName, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NgStyle, FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [NgStyle, FormsModule, ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -13,7 +14,7 @@ export class LoginComponent {
   isPressed = false;
   myForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router) {
     this.myForm = this.fb.group({
       pw: ['', [Validators.required, Validators.minLength(5)]],
       mail: ['', [Validators.required, Validators.email]],
