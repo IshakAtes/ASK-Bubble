b40076b9d15bee538c12fb4041ee793bc7d3eaf2
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-06-03 14:57:45 +0200

    finished dialog-choose-avatar component Styling

--- src/app/dialog-choose-avatar/dialog-choose-avatar.component.ts
+++ src/app/dialog-choose-avatar/dialog-choose-avatar.component.ts
@@ -1,14 +1,33 @@
+import { NgFor, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-dialog-choose-avatar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor, NgStyle],
   templateUrl: './dialog-choose-avatar.component.html',
   styleUrl: './dialog-choose-avatar.component.scss'
 })
+
 export class DialogChooseAvatarComponent {
+  images: string[] = [
+    '../../assets/img/defaultAvatars/defaultFemale1.png',
+    '../../assets/img/defaultAvatars/defaultMale1.png',
+    '../../assets/img/defaultAvatars/defaultMale4.png',
+    '../../assets/img/defaultAvatars/defaultMale2.png',
+    '../../assets/img/defaultAvatars/defaultFemale2.png',
+    '../../assets/img/defaultAvatars/defaultMale3.png'
+  ];
+  user: any;
   constructor(private router: Router) {}
+
+  selectDummyAvatar(item: any) {
+    console.log(item);
+  }
+
+  uploadAvatarImage() {}
 
 }
