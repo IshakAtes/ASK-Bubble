cc09723b746b7ac6ee543942604ba3eb12f56ded
Kerim <kerim.tasci@yahoo.de>
2024-06-04 11:31:59 +0200

    thread html & css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { DialogChooseAvatarComponent } from './dialog-choose-avatar/dialog-choose-avatar.component';
+import { ThreadComponent } from './thread/thread.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
@@ -14,5 +15,6 @@ export const routes: Routes = [
     { path: 'imprint', component: ImprintComponent},
     { path: 'privacyPolicy', component: PrivacyPolicyComponent},
     { path: 'signUp', component: SignUpComponent},
-    { path: 'choosingAvatar', component: DialogChooseAvatarComponent}
+    { path: 'choosingAvatar', component: DialogChooseAvatarComponent},
+    { path: 'thread', component: ThreadComponent}
 ];
