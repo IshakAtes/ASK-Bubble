e6fd5d054524ac7b0728564bbd8dd9421280bef6
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-06-20 17:26:03 +0200

    another was for password validation

--- src/app/dialog-change-password/dialog-change-password.component.ts
+++ src/app/dialog-change-password/dialog-change-password.component.ts
@@ -20,7 +20,7 @@ export class DialogChangePasswordComponent implements OnInit {
     this.myForm = this.formBuilder.group({
       newPass1: ['', [Validators.required, Validators.minLength(5)]],
       newPass2: ['', [Validators.required, Validators.minLength(5)]]
-    });
+    }, { validator: this.passwordMatchValidator });
   }
 
   ngOnInit():void {
@@ -28,16 +28,18 @@ export class DialogChangePasswordComponent implements OnInit {
     console.log(this.userId);
   }
 
-  checkPasswords(): boolean {
-    const newPass1 = this.myForm.get('newPass1')?.value;
-    const newPass2 = this.myForm.get('newPass2')?.value;
-    return newPass1 === newPass2;
+
+  passwordMatchValidator(group: FormGroup): { [key: string]: any } | null {
+    const newPass1 = group.get('newPass1')?.value;
+    const newPass2 = group.get('newPass2')?.value;
+    return newPass1 === newPass2 ? null : { passwordMismatch: true }; // Rückgabe eines Fehlers, wenn die Passwörter nicht übereinstimmen
   }
+
 
   onSubmit() {
     if (this.myForm.valid) {
       // Password change logic here
-      console.log('Form submitted', this.myForm.value, this.checkPasswords());
+      console.log('Form submitted', this.myForm.value);
     } else {
       console.log('Form not valid');
     }
